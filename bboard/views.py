bf148be6f5ee6526b79b089105d9df8133e5b7de
Sadburritos <Pochtagleba@bk.ru>
2024-01-31 21:32:58 +0600

    homework_03

--- bboard/views.py
+++ bboard/views.py
@@ -1,35 +1,175 @@
+from django.core.exceptions import NON_FIELD_ERRORS
+from django.core.paginator import Paginator
+from django.db.models import Count
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import RedirectView
+from django.views.generic.dates import ArchiveIndexView, MonthArchiveView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .forms import BbForm
 from .models import Bb, Rubric
 
 
 def index(request):
-    bbs = Bb.objects.order_by('-published')
-    rubrics = Rubric.objects.all()
-    context = {'bbs': bbs, 'rubrics': rubrics}
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.annotate(cnt=Count('bb')).filter(cnt__gt=0)
+
+    paginator = Paginator(bbs, 2, orphans=2)
+    # paginator = Paginator(bbs, 1, orphans=0)
+
+    if 'page' in request.GET:
+        page_num = request.GET['page']
+    else:
+        page_num = 1
+
+    page = paginator.get_page(page_num)
+
+    context = {'rubrics': rubrics, 'page_obj': page, 'bbs': page.object_list}
+
     return render(request, 'index.html', context)
 
 
-def by_rubric(request, rubric_id):
-    bbs = Bb.objects.filter(rubric=rubric_id)
-    rubrics = Rubric.objects.all()
-    current_rubric = Rubric.objects.get(pk=rubric_id)
-    context = {'bbs': bbs, 'rubrics': rubrics,
-               'current_rubric': current_rubric}
-    return render(request, 'by_rubric.html', context)
+class BbIndexView(ListView):
+    model = Bb
+    template_name = 'index.html'
+    context_object_name = 'bbs'
+    paginate_by = 2
+    paginate_orphans = 2
+
+    def get_queryset(self):
+        return Bb.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.annotate(cnt=Count('bb')).filter(cnt__gt=0)
+        return context
 
 
-class BbCreateView(CreateView):
-    template_name = 'create.html'
-    form_class = BbForm
-    success_url = reverse_lazy('index')
+# class BbIndexView(ArchiveIndexView):
+#     model = Bb
+#     template_name = 'index.html'
+#     date_field = 'published'
+#     date_list_period = 'month'
+#     context_object_name = 'bbs'
+#     allow_empty = True
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['rubrics'] = Rubric.objects.annotate(cnt=Count('bb')).filter(cnt__gt=0)
+#         return context
+
+
+class BbMonthView(MonthArchiveView):
+    model = Bb
+    template_name = 'index.html'
+    date_field = 'published'
+    date_list_period = 'month'
+    month_format = '%m'
+    context_object_name = 'bbs'
+    allow_empty = True
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+
+class BbRedirectView(RedirectView):
+    url = '/'
+
+
+class BbByRubricView(ListView):
+    template_name = 'by_rubric.html'
+    context_object_name = 'bbs'
+
+    def get_queryset(self):
+        return Bb.objects.filter(rubric=self.kwargs['rubric_id'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        context['current_rubric'] = Rubric.objects.get(
+            pk=self.kwargs['rubric_id'])
+        return context
+
+
+class BbDetailView(DetailView):
+    model = Bb
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['rubrics'] = Rubric.objects.all()
         return context
+
+
+class BbCreateView(CreateView):
+    template_name = 'create.html'
+    form_class = BbForm
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+
+class BbEditView(UpdateView):
+    model = Bb
+    form_class = BbForm
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubric'] = Rubric.objects.all()
+        return context
+
+
+class BbDeleteView(DeleteView):
+    model = Bb
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubric'] = Rubric.objects.all()
+        return context
+
+
+def edit(request, pk):
+    bb = Bb.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        bbf = BbForm(request.POST, instance=bb)
+        if bbf.is_valid():
+            if bbf.has_changed():
+                bbf.save()
+            return HttpResponseRedirect(
+                # reverse('bboard:by_rubric',
+                #         kwargs={'rubric_id': bbf.cleaned_data['rubric'].pk})
+                reverse('bboard:detail',
+                        kwargs={'pk': pk})
+            )
+        else:
+            context = {'form': bbf}
+            return render(request, 'bboard/bb_form.html', context)
+    else:
+        bbf = BbForm(instance=bb)
+        context = {'form': bbf}
+        return render(request, 'bboard/bb_form.html', context)
+
+
+def add_save(request):
+    bbf = BbForm(request.POST)
+
+    if bbf.is_valid():
+        bbf.save()
+        return HttpResponseRedirect(
+            reverse('bboard:by_rubric',
+                    kwargs={'rubric_id': bbf.cleaned_data['rubric'].pk})
+        )
+    else:
+        context = {'form': bbf}
+        return render(request, 'bboard/bb_form.html', context)
--- samplesite/settings.py
+++ samplesite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'bboard.apps.BboardConfig',  # 'bboard',
+    'testapp',
 ]
 
 MIDDLEWARE = [
@@ -56,14 +57,21 @@ ROOT_URLCONF = 'samplesite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        # 'BACKEND': 'django.template.backends.jinja2.Jinja2',
         'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
+            # 'file_charset': 'utf-8',
+            # 'debug': False,
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
+                'django.contrib.auth.context_processors.auth',  # user, perms
                 'django.contrib.messages.context_processors.messages',
+
+                # 'django.template.context_processors.csrf',
+                'django.template.context_processors.static',
+                # 'django.template.context_processors.media',
             ],
         },
     },
@@ -117,10 +125,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+# STATICFILES_DIRS = [
+#     ('main', BASE_DIR / 'static'),
+# ]
+
+# STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
